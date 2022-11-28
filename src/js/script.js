c482592bc43a9df996a01ab903524122f2032b36
Maqel <98158452+Maqel@users.noreply.github.com>
2022-11-28 22:57:33 +0100

    all_animations_work

--- src/js/script.js
+++ src/js/script.js
@@ -5,28 +5,23 @@
   let playSound = () => new Audio('./vendor/sounds/button-click.wav').play();
   //let themeSound = () => new Audio('./vendor/sounds/retro-wave.mp3');
 
-  /* RESULTS */
   const printMessage = (msg) => {
     let div = document.createElement('div');
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
   };
 
-  /* CLEAR MESSAGES */
   const clearMessages = () => {
     document.getElementById('messages').innerHTML = '';
   };
 
-  /*HANDS MOVEMENT*/
-  const playerMovement = document.querySelector('.player-move');
-  const computerMovement = document.querySelector('.computer-move');
+  const playerMovement = document.querySelector('.player_move');
+  const computerMovement = document.querySelector('.computer_move');
 
-  /* RESET MOVEMENT */
   function resetMovement() {
-    playerMovement.src = '/.images/rock/rock_left.png';
+    playerMovement.src = './images/rock/rock_left.png';
     computerMovement.src = './images/rock/rock_right.png';
   }
-
   function shake() {
     playerMovement.classList.add('shake');
     computerMovement.classList.add('shake');
@@ -35,7 +30,6 @@
       computerMovement.classList.remove('shake');
     }, 1500);
   }
-
   /* START GAME */
   const startButton = document.querySelector('.intro button');
   const showMenu = document.querySelector('#container');
@@ -47,28 +41,28 @@
 
   const playGame = (playerInput) =>{
 
-    resetMovement();
     clearMessages();
-    const getMoveName = function(argMoveId){
-      if(argMoveId == 1){
+    resetMovement();
+    const getMoveName = (argMoveId) =>{
+      if(argMoveId === 1){
         playerMovement.src = './images/rock/rock_left.png';
         return 'rock';
       }
-      else if(argMoveId == 2){
+      else if(argMoveId === 2){
         playerMovement.src = './images/paper/paper_left.png';
         return 'paper';
       }
-      else if(argMoveId == 3){
+      else if(argMoveId === 3){
         playerMovement.src = './images/scissors/scissors_left.png';
         return 'scissors';
       }
-      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-      return 'nieznany ruch';
+      printMessage('I do not know such a movement ' + argMoveId + '.');
+      return 'unknown movement';
     };
 
     //Random Number//
     const randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log('Wylosowana liczba to: ' + randomNumber);
+    //console.log('Wylosowana liczba to: ' + randomNumber);
 
     //Computer Move//
     const argComputerMove = getMoveName(randomNumber);
@@ -77,21 +71,19 @@
 
 
     function replaceComputerImage() {
-      if (argComputerMove === 'Rock') {
+      if (argComputerMove === 'rock') {
         computerMovement.src = './images/rock/rock_right.png';
-      } else if (argComputerMove === 'Paper') {
+      } else if (argComputerMove === 'paper') {
         computerMovement.src = './images/paper/paper_right.png';
-      } else if (argComputerMove === 'Scissors') {
+      } else if (argComputerMove === 'scissors') {
         computerMovement.src = './images/scissors/scissors_right.png';
       }
     }
 
     //Player Move
-    console.log('Gracz wpisał: ' + playerInput);
     const argPlayerMove = getMoveName(playerInput);
-    replaceComputerImage();
-
     printMessage('Your move is: ' + argPlayerMove);
+    replaceComputerImage();
 
     //Winning result//
     const displayResult = (argComputerMove, argPlayerMove) => {
@@ -116,25 +108,27 @@
   };
 
   document.getElementById('rock').addEventListener('click', function(){
-    shake();
     setTimeout(function(){
       playGame(1);
     }, 1500);
-
     playSound();
+    shake();
+
   });
   document.getElementById('paper').addEventListener('click', function(){
-    shake();
     setTimeout(function(){
       playGame(2);
     }, 1500);
     playSound();
+    shake();
   });
   document.getElementById('scissors').addEventListener('click', function(){
-    shake();
     setTimeout(function(){
       playGame(3);
     }, 1500);
     playSound();
+    shake();
   });
+
+
 }
